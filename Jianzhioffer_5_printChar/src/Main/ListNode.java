926bbefc558e9bdded67a8da3fa39484b0b2a41a
linxj <635596741@qq.com>
2015-12-14 00:03:04 +0800

    add the printChar

---
+++ Jianzhioffer_5_printChar/src/Main/ListNode.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class ListNode {
+public int value;
+public ListNode nextNode;
+public ListNode(int value){
+	this.value = value;
+}
+public void display(){
+	System.out.print(value);
+}
+}
